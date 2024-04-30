5971f72dacaa237eae99ede6c0885a4726143390
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2024-04-30 11:22:48 -0300

    feat: add inicial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import { Button } from './ui/button'
+import { MenuIcon } from 'lucide-react'
+const Header = () => {
+    return (
+        <div className="flex justify-between pt-6 px-5">
+            <Image src="/logo.png" alt="FSW Foods" height={30} width={100}/>
+            <Button className='bg-transparent border-none' size='icon' variant='outline'>
+                <MenuIcon />
+            </Button>
+        </div>
+    );
+}
+ 
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>layout</h1>
-        {children}
+ddd        {children}
       </body>
     </html>
   );
