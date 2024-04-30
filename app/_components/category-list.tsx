15a95643086f0a54c404cae9e09dbf79a8beb638
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2024-04-30 11:36:26 -0300

    fix: add prisma file

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,8 @@
+const CategoryList = () => {
+    return ( 
+        // pegar as categorias do bnacno de dados
+        // renderizar um item para cada categoria
+     );
+}
+ 
+export default CategoryList;
